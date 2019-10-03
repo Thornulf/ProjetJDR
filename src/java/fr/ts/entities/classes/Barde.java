206bbc43707e8a168811ff5da39af1cad35a4038
Thomas <samson.thoma@gmail.com>
2019-10-03 11:52:11 +0200

    Ajout des constructeurs plein

--- src/java/fr/ts/entities/classes/Barde.java
+++ src/java/fr/ts/entities/classes/Barde.java
@@ -30,6 +30,17 @@ public final class Barde extends Personnage{
         this.charisme = this.setCharisme(charisme);
     } 
 
+    public Barde(int idPersonnage, String nom, String prenom, int vie, int force, int dexterite, int intelligence, int constitution, int sagesse, int charisme, int experience, int idUtilisateur, int idClasse, int niveaux) {
+        super(idPersonnage, nom, prenom, vie, force, dexterite, intelligence, constitution, sagesse, charisme, experience, idUtilisateur, idClasse, niveaux);
+        this.vie = this.setVie(vie);
+        this.force = this.setForce(force);
+        this.dexterite = this.setDexterite(dexterite);
+        this.intelligence = this.setIntelligence(intelligence);
+        this.constitution = this.setConstitution(constitution);
+        this.sagesse = this.setSagesse(sagesse);
+        this.charisme = this.setCharisme(charisme);
+    }
+    
     @Override
     public int setVie(int vie) {
         if(vie > BARDE_PV_MAX) {
--- src/java/fr/ts/entities/classes/Pretre.java
+++ src/java/fr/ts/entities/classes/Pretre.java
@@ -30,6 +30,17 @@ public final class Pretre extends Personnage {
         this.charisme = this.setCharisme(charisme);
     }
 
+    public Pretre(int idPersonnage, String nom, String prenom, int vie, int force, int dexterite, int intelligence, int constitution, int sagesse, int charisme, int experience, int idUtilisateur, int idClasse, int niveaux) {
+        super(idPersonnage, nom, prenom, vie, force, dexterite, intelligence, constitution, sagesse, charisme, experience, idUtilisateur, idClasse, niveaux);
+        this.vie = this.setVie(vie);
+        this.force = this.setForce(force);
+        this.dexterite = this.setDexterite(dexterite);
+        this.intelligence = this.setIntelligence(intelligence);
+        this.constitution = this.setConstitution(constitution);
+        this.sagesse = this.setSagesse(sagesse);
+        this.charisme = this.setCharisme(charisme);
+    }
+
     @Override
     public int setVie(int vie) {
         if (vie > PRETRE_PV_MAX) {
